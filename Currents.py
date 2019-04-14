5fcb86f42f410986a4c6748849784b5633e25fc2
Ariadne Thompson <43646932+AriadneThompson@users.noreply.github.com>
2019-04-14 10:23:46 +0100

    Updated currents and added example trajectories

--- Currents.py
+++ Currents.py
@@ -30,6 +30,32 @@ LONG_LOWER_LIMIT = -19
 
 
 
+def radial_to_euclidean(theta, r):
+    # Radial data is relative to North; should be relative to x axis (latitude)
+    theta = (theta + 90) % 360
+    theta *= - np.pi / 180
+    return r * np.cos(theta), r * np.sin(theta)
+
+
+
+def plot_vector_field(velocity_field):
+    lat_grid = np.arange(LAT_LOWER_LIMIT, LAT_UPPER_LIMIT+1)
+    long_grid = np.arange(LONG_LOWER_LIMIT, LONG_UPPER_LIMIT+1)
+    
+    fig, ax = plt.subplots()
+    ax.quiver(long_grid, lat_grid, velocity_field["long_vel"][:,:,0], velocity_field["lat_vel"][:,:,0], scale=2.5)
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+    
+    plt.show()
+    
+    
+    
+def trajectory(velocity_field, long_0, lat_0):
+    
+
+
+
 f = open("data.txt", "r")
 
 for line in f:
@@ -37,7 +63,32 @@ for line in f:
 
 
 
-currents = np.dtype([("direction", np.float64), ("speed", np.float64), ("long", np.float64), ("lat", np.float64), ("time", np.float64)])
+# Longitudinal and latitudinal components of velocity. 
+current = np.dtype([("long_vel", np.float64), ("lat_vel", np.float64)])
+
+
+lat_grid_size = LAT_UPPER_LIMIT - LAT_LOWER_LIMIT + 1
+long_grid_size = LONG_UPPER_LIMIT - LONG_LOWER_LIMIT + 1
+time_grid_size = 8
+
+velocity_field = np.empty(shape=(lat_grid_size, long_grid_size, time_grid_size), dtype=current)
+
+i = 0
+
+for long in data.keys():
+    j = 0
+    
+    for lat in data[long].keys():
+        for k in range(8):
+            velocity_field[i][j][k]["lat_vel"], velocity_field[i][j][k]["long_vel"] = radial_to_euclidean(data[long][lat][k]["currentDirection"], data[long][lat][k]["currentSpeed"])
+        j += 1
+    i += 1
+
+
+
+plot_vector_field(velocity_field)
+
+
 
 
 
